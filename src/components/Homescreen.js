1d554e230e9b151ea46ded13fbdc108945c415f2
Akampurira Brian David <akampsbrio@gmail.com>
2022-10-26 16:10:17 +0300

    Stable Build

---
+++ src/components/Homescreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav'
+const Homescreen = () => {
+  return (
+    <div class="homeScreen">
+        {/* Nav */}
+        <Nav/>
+        {/* Banner */}
+        {/* Row  */}
+    </div>
+  )
+}
+
+export default Homescreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDoj3nAMS_HmmyQAZeNRc5gIHe5z1N1Yu0",
+    authDomain: "netflix-clone-akamps.firebaseapp.com",
+    projectId: "netflix-clone-akamps",
+    storageBucket: "netflix-clone-akamps.appspot.com",
+    messagingSenderId: "89938378916",
+    appId: "1:89938378916:web:8541406bcd0a15f3dca631"
+  };
